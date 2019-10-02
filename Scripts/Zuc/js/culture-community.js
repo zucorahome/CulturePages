cd97882a018ec9c35ae799a457f67848fb63a735
Himadri <hvyas738@gmail.com>
2019-10-02 16:48:25 -0400

    working on gallery

---
+++ Scripts/Zuc/js/culture-community.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+
+// create a function in about us to call tip function on only about us page. and ther are some updates before sending all other page updates. 
+
+console.log("DOM function called");
+});
